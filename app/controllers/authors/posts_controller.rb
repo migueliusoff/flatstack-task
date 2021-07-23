9bf89dce3d64012d9e2c2b34afd635a641bd7a21
Danil Galeev <galoffdan@gmail.com>
2021-07-23 12:14:20 +0300

    try to add addition elements to article

--- app/controllers/authors/posts_controller.rb
+++ app/controllers/authors/posts_controller.rb
@@ -16,7 +16,11 @@ module Authors
     end
 
     # GET /posts/1/edit
-    def edit; end
+    def edit
+      @paragraph = @post.elements.build(element_type: 'paragraph')
+    end
+
+
 
     # POST /posts
     def create
